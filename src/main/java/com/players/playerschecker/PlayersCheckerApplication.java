d2bdfc28724adc0ffeedce4d8e8c727df9dd5df3
jsalazare <javi_9315@hotmail.com>
2021-03-02 19:32:48 -0700

    init commit

---
+++ src/main/java/com/players/playerschecker/PlayersCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.players.playerschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersCheckerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/players/playerschecker/PlayersCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.players.playerschecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
